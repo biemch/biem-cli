d001f52876c39fb35bdd185f113f2d4e3cea0a21
Michael Freund <michael@biem.ch>
2025-01-19 14:15:44 +0100

    feat: enhance environment variable validation

--- src/shared/model/env.model.ts
+++ src/shared/model/env.model.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const EnvSchema = z.object({
-	URL: z.string(),
-	EMAIL: z.string(),
-	PASSWORD: z.string(),
+	URL: z.string().url().nonempty(),
+	EMAIL: z.string().email().nonempty(),
+	PASSWORD: z.string().min(8).nonempty(),
 });
 
 export type Env = z.infer<typeof EnvSchema>;
